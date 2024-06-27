a480258d460943c9d0b93992676c2309308fa1de
Ozeco-Mmem <open305045@gmail.com>
2024-06-28 02:30:31 +0900

    7~9
    # 7. Animation Blueprints
    - 캐릭터 애니메이션 블루프린트 설정
    # 8. Enhanced Input
    - 사용자 입력 액션과 맵핑컨텍스트 추가
    # 9. Aura Player Controller
    - 입력 처리를 위한 플레이어 컨트롤러 추가
    - Enhanced Input 사용을 위해서 모듈 추가

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,32 @@
+// Copyright PandoraCube
+
+
+#include "Player/AuraPlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext);		// Check AuraContext and when it doesn't, get CRASH
+
+	// Get Input Subsystem for Local player
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	// Cursor Setting
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
